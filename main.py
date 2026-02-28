da1c9d7222f9f16c8744c7cb03dab96fbee38501
Leonid <lekara@ttu.ee>
2026-02-28 11:44:46 +0200

    Basic ETL done

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import webbrowser
+import os
+
+daily_rates = pd.read_csv("https://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip", compression="zip")
+historical_rates = pd.read_csv("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip", compression="zip")
+
+def mean_exchange_rate(rates_list):
+    return sum(rates_list) / len(rates_list)
+    
+data = {
+    "USD": [daily_rates[" USD"][0], mean_exchange_rate(historical_rates["USD"].tolist())],
+    "JPY": [daily_rates[" JPY"][0], mean_exchange_rate(historical_rates["JPY"].tolist())],
+    "GBP": [daily_rates[" GBP"][0], mean_exchange_rate(historical_rates["GBP"].tolist())],
+    "SEK": [daily_rates[" SEK"][0], mean_exchange_rate(historical_rates["SEK"].tolist())],
+}
+
+df = pd.DataFrame(data, index=["Today's Rate", "Average Rate"]).T  # transpose
+html = df.to_html(border=1)
+
+with open("exchange_rates.html", "w") as file:
+    file.write(html)
+    
+file_path = os.path.abspath("exchange_rates.html")
+webbrowser.open(f"file://{file_path}")
